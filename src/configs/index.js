4ba330df569e2700a4dce13458b6873a6e740bd4
devriwirlandika <wirlandikadevri@gmail.com>
2022-04-23 12:53:06 +0700

    eslint init

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,11 +1,11 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg')
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  name: process.env.DB_NAME,
-  port: 5432,
-});
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    name: process.env.DB_NAME,
+    port: 5432,
+})
 
-module.exports = pool;
+module.exports = pool

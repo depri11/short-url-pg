a083047a2b275241c7e93e02bc94add5680d6fbb
devriwirlandika <wirlandikadevri@gmail.com>
2022-04-23 12:34:03 +0700

    init db postgres

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const app = express();
+const db = require("./src/configs/");
+const port = process.env.PORT || 4000;
+
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", (req, res) => {
+  res.send("Hello world");
+});
+
+db.connect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Running at port ${port}`);
+    });
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
